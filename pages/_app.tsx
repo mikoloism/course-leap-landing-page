38431c28a1e579b5c635b05919e587ba7732fcaa
miko-github <mikoloism-github@gmail.com>
2022-09-01 20:26:50 +0430

    fix(page): add `viewport` of `meta` tags as initial and default

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import '../node_modules/normalize.css';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
