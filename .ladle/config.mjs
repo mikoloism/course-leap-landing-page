74ea4791ec59448b6add4e577731e795353b439e
miko-github <mikoloism-github@gmail.com>
2022-09-03 15:31:45 +0430

    test(story): fix and replace the `style` element by `meta` element and `viewport` name in `.ladle/config.mjs`

--- .ladle/config.mjs
+++ .ladle/config.mjs
@@ -1,4 +1,4 @@
 export default {
     stories: ['./components/**/*.stories.tsx', './pages/**/*.stories.tsx'],
-    appendToHead: '<style>.ladle-aside { order: -1; }</style>',
+    appendToHead: /*html*/ `<meta name="viewport" content="width=device-width, initial-scale=1" />`,
 };
