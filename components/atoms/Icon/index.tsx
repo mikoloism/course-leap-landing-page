aa6796fe903e55928cb335abec17e6c6a4a072fa
miko-github <mikoloism-github@gmail.com>
2022-09-05 21:47:38 +0430

    feat(component): import icons to `components/atoms/icon/index.tsx` to `IconComponents` as component map

--- components/atoms/Icon/index.tsx
+++ components/atoms/Icon/index.tsx
@@ -1,9 +1,15 @@
-function SVGIcon() {
-    return <strong>Hello</strong>;
-}
+import { ArrowRightIcon } from './ArrowRight';
+import { CheckUnderlineIcon } from './CheckUnderline';
+import { ChevronRightIcon } from './ChevronRight';
+import { CreditCardIcon } from './CreditCard';
+import { TagIcon } from './Tag';
 
 export const IconComponents = {
-    home: SVGIcon,
+    'arrow-right': ArrowRightIcon,
+    'check-underline': CheckUnderlineIcon,
+    'chevron-right': ChevronRightIcon,
+    'credit-card': CreditCardIcon,
+    tag: TagIcon,
 };
 
 export * from './hooks';
