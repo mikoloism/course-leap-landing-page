77e8436b2b0ad471a2547c9490712bcd1f388f35
miko-github <mikoloism-github@gmail.com>
2022-09-05 21:46:10 +0430

    fix(config): add `transparent` and `currentColor` to `tailwind.config.js`

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,11 @@ module.exports = {
     ],
     theme: {
         colors: {
-            primary  : '#23A6F0',
-            secondary: '#252B42',
-            gray     : {
+            transparent : 'transparent',
+            currentColor: 'currentColor',
+            primary     : '#23A6F0',
+            secondary   : '#252B42',
+            gray        : {
                 100    : '#FAFAFA' /* copyright-background */,
                 200    : '#F9F9F9' /* input-background */,
                 300    : '#E6E6E6' /* input-border */,
