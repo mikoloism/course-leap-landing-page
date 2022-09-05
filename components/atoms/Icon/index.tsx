fcf9b56571bb181aefb1808476588dc9fc48dab7
miko-github <mikoloism-github@gmail.com>
2022-09-05 21:38:36 +0430

    feat(component): add four usualy icons; `arrow-right`, `chevron-right`, `check-underline`, `tag` and `credit-card`;

---
+++ components/atoms/Icon/index.tsx
@@ -0,0 +1,11 @@
+function SVGIcon() {
+    return <strong>Hello</strong>;
+}
+
+export const IconComponents = {
+    home: SVGIcon,
+};
+
+export * from './hooks';
+export { default } from './Icon';
+export * from './types';
