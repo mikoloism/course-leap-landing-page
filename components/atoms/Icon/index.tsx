8f07da324b2ccfb54f7958328e4b804ef1904f36
miko-github <mikoloism-github@gmail.com>
2022-09-07 10:29:44 +0430

    feat(component): add new icon names and components to `icon-components` map object; `components/atoms/Icon/`;

--- components/atoms/Icon/index.tsx
+++ components/atoms/Icon/index.tsx
@@ -1,15 +1,40 @@
+import { AlarmIcon } from './Alarm';
 import { ArrowRightIcon } from './ArrowRight';
+import { BookmarkIcon } from './Bookmark';
+import { BookOpenIcon } from './BookOpen';
 import { CheckUnderlineIcon } from './CheckUnderline';
 import { ChevronRightIcon } from './ChevronRight';
 import { CreditCardIcon } from './CreditCard';
+import { DownloadBoxIcon } from './DownloadBox';
+import { EyeIcon } from './Eye';
+import { HeartIcon } from './Heart';
+import { InstagramIcon } from './Instagram';
+import { LinearChartIcon } from './LinearChart';
+import { LinkedInIcon } from './Linkedin';
+import { ShoppingCartIcon } from './ShoppingCart';
+import { StarFillIcon, StarIcon } from './Star';
 import { TagIcon } from './Tag';
+import { TwitterIcon } from './Twitter';
 
 export const IconComponents = {
-    'arrow-right': ArrowRightIcon,
+    alarm            : AlarmIcon,
+    'arrow-right'    : ArrowRightIcon,
+    bookmark         : BookmarkIcon,
+    'book-open'      : BookOpenIcon,
     'check-underline': CheckUnderlineIcon,
-    'chevron-right': ChevronRightIcon,
-    'credit-card': CreditCardIcon,
-    tag: TagIcon,
+    'chevron-right'  : ChevronRightIcon,
+    'credit-card'    : CreditCardIcon,
+    'download-box'   : DownloadBoxIcon,
+    eye              : EyeIcon,
+    heart            : HeartIcon,
+    instagram        : InstagramIcon,
+    'linear-chart'   : LinearChartIcon,
+    linkedin         : LinkedInIcon,
+    'shopping-cart'  : ShoppingCartIcon,
+    'star-fill'      : StarFillIcon,
+    star             : StarIcon,
+    tag              : TagIcon,
+    twitter          : TwitterIcon,
 };
 
 export * from './hooks';
