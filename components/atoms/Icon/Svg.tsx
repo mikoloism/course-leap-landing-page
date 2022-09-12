42fe4e734256e44bcd324b1b6e6de84e15f394fe
mikoloism <mikoloism@gmail.com>
2022-09-12 13:56:03 +0430

    fix(component): replace passed `icon-svg` file comp as object by react component tag style; add default props to value (default pass as props not work);

--- components/atoms/Icon/Svg.tsx
+++ components/atoms/Icon/Svg.tsx
@@ -2,18 +2,21 @@ interface SvgProps {
     scale?: number | string;
 }
 
-export const DEFAULT_SVG_ICON_PROPS = { scale: 0.7 };
+export const DEFAULT_SVG_ICON_PROPS = { scale: '0.70' };
 
 export const withSvg = (SvgPath: any) => {
-    return function SvgWrapper({ scale }: SvgProps = DEFAULT_SVG_ICON_PROPS) {
+    return function SvgWrapper({ scale }: SvgProps) {
         return (
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="48"
                 height="48"
-                style={{ transform: `scale(${scale})` }}>
-                {/* // TODO : if (not-work) => replace with jsx format <SvgPath />  */}
-                {SvgPath}
+                style={{
+                    transform: `scale(${
+                        scale ?? DEFAULT_SVG_ICON_PROPS.scale
+                    })`,
+                }}>
+                <SvgPath />
             </svg>
         );
     };
