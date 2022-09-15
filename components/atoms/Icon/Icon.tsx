1677b4c25ca03855e32fedcd0b2876f8d3620d91
mikoloism <mikoloism@gmail.com>
2022-09-15 22:23:01 +0430

    fix(component): add default classnames to `icon` component (@/atoms/Icon/);

--- components/atoms/Icon/Icon.tsx
+++ components/atoms/Icon/Icon.tsx
@@ -1,3 +1,4 @@
+import { $ } from '@/libs/common';
 import { Component } from 'react';
 import { getIconComponent } from './hooks';
 import type { IconProps } from './types';
@@ -8,11 +9,17 @@ export default class Icon extends Component<IconProps, any, any> {
     }
 
     public render() {
-        const { name: $name, scale, ...$props } = this.props;
+        const { name: $name, scale, className, ...$props } = this.props;
+        const $className = $(
+            'inline-flex flex-wrap flex-row place-content-center place-items-center',
+            className
+        );
         const IconComponent = getIconComponent($name);
 
         return (
-            <figure {...$props}>
+            <figure
+                className={$className}
+                {...$props}>
                 <IconComponent scale={scale} />
             </figure>
         );
