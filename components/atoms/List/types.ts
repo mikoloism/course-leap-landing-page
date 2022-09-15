32a1d66306ecdceb6dec40d5d193eb9a7f6fc17d
mikoloism <mikoloism@gmail.com>
2022-09-15 20:28:23 +0430

    feat(type): add `types` file to `list` component (@/atoms/list);

---
+++ components/atoms/List/types.ts
@@ -0,0 +1,35 @@
+// special-react-props (aliased `key`)
+export type AsKey =
+    | string
+    | number
+    | Array<string | number>
+    | ((item: any[], index: number) => string | number);
+
+export interface ItemType {
+    [key: string]: any;
+}
+
+export type FlexDirection =
+    | 'vertical'
+    | 'vertical-reverse'
+    | 'horizontal'
+    | 'horizontal-reverse'
+    | 'row'
+    | 'col'
+    | 'column'
+    | 'row-reverse'
+    | 'col-reverse'
+    | 'column-reverse';
+
+export type FlexAlign = 'center' | 'start' | 'end';
+
+export interface StyleProps {
+    nowrap?: boolean;
+    dir?: FlexDirection;
+    placeItems?: FlexAlign;
+    placeContent?: FlexAlign;
+
+    // gap-[x|y] is tailwindcss (gap-x-* | gap-y-*)
+    gapx?: string | number;
+    gapy?: string | number;
+}
