72a67465ec7816ee5db09baca2f878334259af49
mikoloism <mikoloism@gmail.com>
2022-09-15 20:50:36 +0430

    refactor(component): include `image` props type in file component;
    export as not-default; include `next/image` component; fix styling;

--- components/atoms/Image/Image.tsx
+++ components/atoms/Image/Image.tsx
@@ -1,21 +1,46 @@
-import { Component, memo, Image as NextImage } from '@/libs/common';
-import { ImageProps } from './index';
+import { Component, memo, Image as NextImage, $ } from '@/libs/common';
 
-class Image extends Component<ImageProps> {
+interface ImageProps {
+    src: string;
+    alt: string;
+    style?: object;
+    className?: string;
+}
+
+export class Image extends Component<ImageProps> {
     constructor(props: ImageProps) {
         super(props);
     }
 
     render() {
+        const $className = $(
+            'relative m-0 p-0',
+            'place-content-center',
+            'place-items-center',
+            this.props.className
+        );
+
         return (
-            <figure className={this.props.className}>
+            <figure
+                className={$className}
+                style={{
+                    display: 'grid',
+                    gridTemplateColumns: 'repeat(12, 1fr)',
+                    gridTemplateRows: 'repeat(12, 1fr)',
+                }}>
                 <NextImage
                     src={this.props.src}
-                    layout="intrinsic"
+                    alt={this.props.alt}
+                    width="100%"
+                    height="100%"
+                    layout="responsive"
+                    objectFit="cover"
+                    style={this.props.style ?? {}}
+                    priority
                 />
             </figure>
         );
     }
 }
 
-export default memo(Image);
+// export default memo(Image);
