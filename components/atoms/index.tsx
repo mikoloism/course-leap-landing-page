6e66b985fd32a95a1cb5de678f373491660ed464
mikoloism <mikoloism@gmail.com>
2022-09-22 15:20:25 +0330

    feat(component): re-export `atoms/` component as single file as `index.tsx`

---
+++ components/atoms/index.tsx
@@ -0,0 +1,17 @@
+/** Core Atoms (7 component)
+ * Button (need change)
+ * Group (need change)
+ * Icon
+ * Image
+ * Link
+ * List (need change)
+ * Text (need change)
+ */
+
+export * from './Button';
+export * from './Group';
+export * from './Icon';
+export * from './Image';
+export * from './Link';
+export * from './List';
+export * from './Text';
