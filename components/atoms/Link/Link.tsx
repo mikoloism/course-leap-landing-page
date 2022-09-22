324c873dd8bf2cc146802eef500a1029fb3435b3
mikoloism <mikoloism@gmail.com>
2022-09-22 15:24:30 +0330

    fix(component): add `classname` and passed `classNames` to `Link.tsx` component;

--- components/atoms/Link/Link.tsx
+++ components/atoms/Link/Link.tsx
@@ -1,10 +1,11 @@
-import type { ReactNode } from '@/libs/common';
+import { $, ReactNode } from '@/libs/common';
 import { Component, Link as NextLink } from '@/libs/common';
 
 interface LinkProps {
     className?: string;
     children: ReactNode | JSX.Element;
     href: string;
+    overlay?: boolean;
 }
 
 export class Link extends Component<LinkProps> {
@@ -13,11 +14,15 @@ export class Link extends Component<LinkProps> {
     }
 
     render() {
+        const $className = $(
+            this.props.overlay &&
+                'absolute top-0 bottom-0 left-0 right-0 w-full h-full opacity-0 block z-50',
+            this.props.className
+        );
+
         return (
             <NextLink href={this.props.href}>
-                <a className={this.props.className ?? ''}>
-                    {this.props.children}
-                </a>
+                <a className={$className}>{this.props.children}</a>
             </NextLink>
         );
     }
