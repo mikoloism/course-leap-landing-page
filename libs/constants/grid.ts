bcaefdd96ba4e0ebb2b9336329dde441ed7b3891
mikoloism <mikoloism@gmail.com>
2022-09-23 14:53:09 +0330

    feat(util): add `grid` tailwindcss classNames constants and function to re-use in grid display props;

---
+++ libs/constants/grid.ts
@@ -0,0 +1,17 @@
+import { GridType } from '../types/style';
+export type ExactGridDisplay = 'grid' | 'inline-grid';
+const gridType = ['flex', 'inline-flex', 'in-flex'];
+
+export function isGrid(type: string): type is GridType {
+    return gridType.includes(type);
+}
+
+export const GRID_DISPLAY: Record<GridType, ExactGridDisplay> = {
+    grid: 'grid',
+    'in-grid': 'inline-grid',
+    'inline-grid': 'inline-grid',
+};
+
+export function getGridDisplay(type: GridType): ExactGridDisplay {
+    return GRID_DISPLAY[type];
+}
---
+++ libs/constants/index.ts
@@ -0,0 +1,5 @@
+export * from './align';
+export * from './flex';
+export * from './gap';
+export * from './grid';
+export * from './position';
