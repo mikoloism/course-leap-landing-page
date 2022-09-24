bf1d7b7d3df8498c385649d5ad0c3ec30ec1efa0
mikoloism <mikoloism@gmail.com>
2022-09-24 16:50:09 +0330

    refactor(hook): rename `getAsKey` normalizer hook to `getKeyProperty`; use object destructuring params instead classikc params;
    remove `getDirection`, `getPlaceContent`, `getPlaceItems` and `getStyle` (used `libs/constants/` instead);

--- components/atoms/List/hooks.ts
+++ components/atoms/List/hooks.ts
@@ -1,84 +1,26 @@
-import { $ } from '@/libs/common';
-import type {
-    AsKey,
-    FlexAlign,
-    FlexDirection,
-    ItemType,
-    StyleProps,
-} from './types';
+import type { KeyType, DataType } from './types';
 
-export function getAsKey(
-    key: AsKey | undefined,
-    index: number,
-    item: ItemType
-): string | number {
-    if (typeof key === 'string') {
-        return item[key];
-    }
+interface GetKeyProperty<T> {
+    key: KeyType<T> | undefined;
+    value: DataType<T> | undefined;
+    index: number;
+}
+type GetKeyResult = string | number;
 
-    if (key instanceof Array) {
-        return key[index];
-    }
+export function getKeyProperty<T>({
+    key,
+    value,
+    index,
+}: GetKeyProperty<T>): GetKeyResult {
+    if (!key) return index;
+
+    if (typeof key === 'string') return (value as Record<any, any>)[key];
+
+    if (key instanceof Array) return key[index];
 
     if (typeof key === 'function' && 'call' in key) {
-        return key.call({}, item as any, index as number);
+        return key.call({}, value as any, index as number);
     }
 
     return index;
-}
-
-export function getDirection(direction: FlexDirection | undefined): string {
-    const cols = ['col', 'column', 'vertical'];
-    const rows = ['row', 'horizontal'];
-    const colsReverse = ['col-reverse', 'column-reverse', 'vertical-reverse'];
-    const rowsReverses = ['row-reverse', 'horizontal-reverse'];
-
-    return direction
-        ? $({
-              'flex-col': cols.includes(direction),
-              'flex-row': rows.includes(direction),
-              'flex-col-reverse': colsReverse.includes(direction),
-              'flex-row-reverse': rowsReverses.includes(direction),
-          })
-        : 'flex-col';
-}
-
-export function getPlaceContent(placeContent: FlexAlign | undefined): string {
-    return placeContent
-        ? $({
-              'place-content-center': placeContent === 'center',
-              'place-content-start': placeContent === 'start',
-              'place-content-end': placeContent === 'end',
-          })
-        : 'place-content-center';
-}
-
-export function getPlaceItems(placeItems: FlexAlign | undefined): string {
-    return placeItems
-        ? $({
-              'place-items-center': placeItems === 'center',
-              'place-items-start': placeItems === 'start',
-              'place-items-end': placeItems === 'end',
-          })
-        : 'place-items-center';
-}
-
-export function getGapX(gap: number | string | undefined) {
-    return 'gap-x-' + gap;
-}
-
-export function getGapY(gap: number | string | undefined) {
-    return 'gap-y-' + gap;
-}
-
-export function getStyle(props: StyleProps): string {
-    return $(
-        'flex list-none',
-        getDirection(props.dir),
-        getPlaceItems(props.placeItems),
-        getPlaceContent(props.placeContent),
-        getGapX(props.gapx),
-        getGapY(props.gapy),
-        { 'flex-wrap': !props.nowrap }
-    );
 }
