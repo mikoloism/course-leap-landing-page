fb711883b8edfd77b3bdd90fde5a4824c4e4cbfd
mikoloism <mikoloism@gmail.com>
2022-09-24 20:28:04 +0330

    fix(util): remove `factory` function for `gap` size (as map-object); replace them by manual wrote `map-object` of `tailwind` classNames;
    import and use `GapType` as type;

--- libs/constants/gap.ts
+++ libs/constants/gap.ts
@@ -1,28 +1,50 @@
+import type { GapType } from '../types';
+
 type x = 'x';
-const x = 'x' as const;
 type y = 'y';
-const y = 'y' as const;
 type XorY = x | y;
+type GapRecord<T extends XorY = x> = Record<GapType, `gap-${T}-${GapType}`>;
 
-const GapSize = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16, 20, 32];
+export const GAP_X: GapRecord<x> = {
+    1: 'gap-x-1',
+    2: 'gap-x-2',
+    3: 'gap-x-3',
+    4: 'gap-x-4',
+    5: 'gap-x-5',
+    6: 'gap-x-6',
+    7: 'gap-x-7',
+    8: 'gap-x-8',
+    9: 'gap-x-9',
+    10: 'gap-x-10',
+    12: 'gap-x-12',
+    14: 'gap-x-14',
+    16: 'gap-x-16',
+    20: 'gap-x-20',
+    32: 'gap-x-32',
+} as const;
+export const GAP_Y: GapRecord<y> = {
+    1: 'gap-y-1',
+    2: 'gap-y-2',
+    3: 'gap-y-3',
+    4: 'gap-y-4',
+    5: 'gap-y-5',
+    6: 'gap-y-6',
+    7: 'gap-y-7',
+    8: 'gap-y-8',
+    9: 'gap-y-9',
+    10: 'gap-y-10',
+    12: 'gap-y-12',
+    14: 'gap-y-14',
+    16: 'gap-y-16',
+    20: 'gap-y-20',
+    32: 'gap-y-32',
+} as const;
 
-type GapRecord<T extends XorY = x> = Record<number, `gap-${T}-${number}`>;
-
-export function createGapMap(type: XorY = x) {
-    return GapSize.reduce(
-        (gaps, size) => Object.assign(gaps, { [size]: `gap-${type}-${size}` }),
-        {} // default-empty-object
-    );
+export function getGapX(gapx?: GapType): string {
+    return gapx ? GAP_X[gapx] : '';
 }
-
-export const GAP_X: GapRecord<x> = createGapMap(x);
-export const GAP_Y: GapRecord<y> = createGapMap(y);
-
-export function getGapX(gapx?: string | number): string {
-    return gapx ? GAP_X[gapx as number] : '';
-}
-export function getGapY(gapy?: string | number): string {
-    return gapy ? GAP_Y[gapy as number] : '';
+export function getGapY(gapy?: GapType): string {
+    return gapy ? GAP_Y[gapy] : '';
 }
 export function getGapSize({ gapx, gapy }: any): string {
     return `${getGapX(gapx)} ${getGapY(gapy)}`;
