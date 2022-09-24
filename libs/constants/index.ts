e2f3dc31907153fdc635d77a8b0e2b13076da772
mikoloism <mikoloism@gmail.com>
2022-09-24 16:05:26 +0330

    feat(util): add `text-[size|color|align]` constants and their function; add `font-weight` constants and function; as `libs/constants/text.ts` file;

--- libs/constants/index.ts
+++ libs/constants/index.ts
@@ -3,3 +3,4 @@ export * from './flex';
 export * from './gap';
 export * from './grid';
 export * from './position';
+export * from './text';
---
+++ libs/constants/text.ts
@@ -0,0 +1,88 @@
+import {
+    AlignPropsType,
+    ColorPropsType,
+    FontWeightType,
+    HeadingElementSize,
+    TextElementSize,
+    TextElementType,
+} from '@/atoms/Text/types';
+
+type TailwindSize = 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl';
+type TextSizeKey =
+    | HeadingElementSize
+    | TextElementSize
+    | TextElementType
+    | TailwindSize;
+
+type ValidTextSize = `text-${Exclude<TextSizeKey, TextElementType>}`;
+export const TEXT_SIZE: Record<TextSizeKey, ValidTextSize> = {
+    h1: 'text-h1',
+    h2: 'text-h2',
+    h3: 'text-h3',
+    h4: 'text-h4',
+    h5: 'text-h5',
+    h6: 'text-h6',
+    standard: 'text-standard',
+    paragraph: 'text-paragraph',
+    link: 'text-link',
+    btn: 'text-btn',
+    subtitle: 'text-subtitle',
+    sm: 'text-sm',
+    md: 'text-md',
+    lg: 'text-lg',
+    xl: 'text-xl',
+    '2xl': 'text-2xl',
+    '3xl': 'text-3xl',
+    span: 'text-standard',
+    strong: 'text-standard',
+    em: 'text-standard',
+    p: 'text-paragraph',
+} as const;
+export function getTextSize(size: TextSizeKey | undefined): ValidTextSize {
+    return TEXT_SIZE[size ?? 'standard'];
+}
+
+type ValidColorType = `text-${ColorPropsType}`;
+export const TEXT_COLOR: Record<ColorPropsType, ValidColorType> = {
+    primary: 'text-primary',
+    secondary: 'text-secondary',
+    accent: 'text-accent',
+    gray: 'text-gray',
+    white: 'text-white',
+    black: 'text-black',
+} as const;
+export function getTextColor(
+    color: ColorPropsType | undefined
+): ValidColorType {
+    return TEXT_COLOR[color ?? 'gray'];
+}
+
+type ValidAlignType = `text-${AlignPropsType}`;
+export const TEXT_ALIGN: Record<AlignPropsType, ValidAlignType> = {
+    center: 'text-center',
+    left: 'text-left',
+    right: 'text-right',
+    justify: 'text-justify',
+} as const;
+export function getTextAlign(
+    align: AlignPropsType | undefined
+): ValidAlignType {
+    return TEXT_ALIGN[align ?? 'justify'];
+}
+
+type ValidFontWeight = `font-${FontWeightType}`;
+export const TEXT_WEIGHT: Record<FontWeightType, ValidFontWeight> = {
+    thin: 'font-thin',
+    light: 'font-light',
+    normal: 'font-normal',
+    medium: 'font-medium',
+    semibold: 'font-semibold',
+    bold: 'font-bold',
+    extrabold: 'font-extrabold',
+    black: 'font-black',
+};
+export function getFontWeight(
+    fontWeight: FontWeightType | undefined
+): ValidFontWeight | '' {
+    return fontWeight ? TEXT_WEIGHT[fontWeight] : '';
+}
