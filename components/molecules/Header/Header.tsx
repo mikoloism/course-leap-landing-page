fcfbc50bb79625e77fc20a83563624ffd18e1211
mikoloism <mikoloism@gmail.com>
2022-09-24 15:57:12 +0330

    feat(component): add `Header` component to `@/components/molecules/`

---
+++ components/molecules/Header/Header.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import {
+    ColorPropsType,
+    HeadingElementType,
+} from '@/components/atoms/Text/types';
+import { Text } from '@/components/atoms';
+
+type HeaderColorType =
+    | ColorPropsType
+    | { title?: ColorPropsType; description?: ColorPropsType };
+
+interface Props
+    extends React.PropsWithChildren<{
+        className?: string;
+        level: HeadingElementType;
+        title: string;
+        description?: string;
+        Before?: any;
+        After?: any;
+        color?: HeaderColorType;
+        width?: number | string;
+    }> {}
+
+function getColor(
+    color: HeaderColorType | undefined,
+    property: 'title' | 'description'
+): ColorPropsType {
+    // no color props passed
+    if (!color) {
+        return property === 'title'
+            ? 'secondary'
+            : 'gray' /* description description color */;
+    }
+
+    // both title and description are same color
+    if (typeof color === 'string') return color;
+
+    // color is object but wasn't set for property
+    if (!color[property]) {
+        return property === 'title'
+            ? 'secondary'
+            : 'gray' /* description default color */;
+    }
+
+    // color is object and set for property
+    return color[property] ?? 'secondary';
+}
+
+function getWidth(width: number | undefined): string {
+    const WIDTH = [
+        '',
+        'w-1/12',
+        'w-2/12',
+        'w-3/12',
+        'w-4/12',
+        'w-5/12',
+        'w-6/12',
+        'w-7/12',
+        'w-8/12',
+        'w-9/12',
+        'w-10/12',
+        'w-11/12',
+        'w-12/12',
+    ];
+
+    return WIDTH[width ?? 11];
+}
+
+export function Header(props: Props) {
+    const BeforeComponent = props.Before;
+    const AfterComponent = props.After;
+    return (
+        <header className={props.className ?? ''}>
+            {BeforeComponent && <BeforeComponent />}
+            <Text
+                as={props.level}
+                color={getColor(props.color, 'title')}>
+                {props.title}
+            </Text>
+            {props.description && (
+                <Text
+                    as="p"
+                    className={getWidth(props.width as number)}
+                    color={getColor(props.color, 'description')}>
+                    {props.description}
+                </Text>
+            )}
+            {AfterComponent && <AfterComponent />}
+        </header>
+    );
+}
