914205166c4a4a338f5056f073267e26ec5f0613
mikoloism <mikoloism@gmail.com>
2022-09-24 16:53:23 +0330

    refactor(type): rename `asKey` to `KeyType` and `KeyProps`; use `generic type` in `KeyType`;
    remove unnecessary types (`FlexDirection` and more); cause their available on `libs/types/`; rename `ItemType` to `DataType`;

--- components/atoms/List/types.ts
+++ components/atoms/List/types.ts
@@ -1,35 +1,20 @@
+type KeyCallbackType<T> = (item: DataType<T>, index: number) => string | number;
+
 // special-react-props (aliased `key`)
-export type AsKey =
+export type KeyType<T> =
     | string
     | number
     | Array<string | number>
-    | ((item: any[], index: number) => string | number);
+    | KeyCallbackType<T>;
 
-export interface ItemType {
-    [key: string]: any;
+export interface KeyProps<T> {
+    $key?: KeyType<T>;
 }
 
-export type FlexDirection =
-    | 'vertical'
-    | 'vertical-reverse'
-    | 'horizontal'
-    | 'horizontal-reverse'
-    | 'row'
-    | 'col'
-    | 'column'
-    | 'row-reverse'
-    | 'col-reverse'
-    | 'column-reverse';
+export type DataType<T extends Record<keyof T, any>> = {
+    [key in keyof T]: T[key];
+};
 
-export type FlexAlign = 'center' | 'start' | 'end';
-
-export interface StyleProps {
-    nowrap?: boolean;
-    dir?: FlexDirection;
-    placeItems?: FlexAlign;
-    placeContent?: FlexAlign;
-
-    // gap[x|y] is tailwindcss (gap-[x|y]-*)
-    gapx?: string | number;
-    gapy?: string | number;
-}
+export type DataProps<T> = {
+    data: Array<DataType<T>>;
+};
