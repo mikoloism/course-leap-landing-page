f8a42878aeb5b250e4ffccde632aa6f9527720bb
mikoloism <mikoloism@gmail.com>
2022-09-24 15:09:14 +0330

    fix(config): add `./libs/constants/**/*.{ts,tsx}` to `content` property in `taillwind.config.js`;
    to create constant classNames as map-className and render with tailwind;

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
     content: [
         './pages/**/*.{js,ts,jsx,tsx}',
         './components/**/*.{js,ts,jsx,tsx}',
+        './libs/constants/**/*.{ts,tsx}',
     ],
     theme: {
         colors: {
