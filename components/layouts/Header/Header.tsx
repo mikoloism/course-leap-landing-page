e271cb40c7033ce8d0caab8372f6fe1b514dc6f0
mikoloism <mikoloism@gmail.com>
2022-09-27 16:35:58 +0330

    style(component): remove `}` symbol from the end of the classNames property

--- components/layouts/Header/Header.tsx
+++ components/layouts/Header/Header.tsx
@@ -12,14 +12,12 @@ export default function Header() {
         <header
             id="header"
             className="grid grid-cols-12 grid-rows-6 w-screen h-36 place-content-center place-items-center bg-secondary text-white text-center font-montserrat px-32 sticky top-0">
-            <HeaderBrand
-                className={`col-start-1 col-end-3 ${$rowClassName}}`}
-            />
+            <HeaderBrand className={`col-start-1 col-end-3 ${$rowClassName}`} />
             <HeaderNavigation
-                className={`col-start-4 col-end-8 ${$rowClassName}}`}
+                className={`col-start-4 col-end-8 ${$rowClassName}`}
             />
             <HeaderActionGroup
-                className={`col-start-10 col-end-13 ${$rowClassName}}`}
+                className={`col-start-10 col-end-13 ${$rowClassName}`}
             />
         </header>
     );
