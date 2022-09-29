00f5385118f9b8e331530fc47537ed0cfca69815
mikoloism <mikoloism@gmail.com>
2022-09-29 21:05:53 +0330

    refactor(component): remove `classNames` library from `layouts/Header` component;
    use `css module` for custom styling;

--- components/layouts/Header/Header.tsx
+++ components/layouts/Header/Header.tsx
@@ -1,17 +1,17 @@
-import { $ } from '@/libs/common';
 import {
     HeaderActionGroup,
     HeaderBrand,
     HeaderNavigation,
 } from '@/organisms/Header';
+import style from '@/styles/header.module.css';
 
 export default function Header() {
-    const $rowClassName = $('row-start-3', 'row-end-7');
+    const $headerClassName =
+        'grid grid-cols-12 grid-rows-6 place-content-center place-items-center w-screen bg-secondary text-white px-32';
+    const $rowClassName = 'row-start-2 row-end-6';
 
     return (
-        <header
-            id="header"
-            className="grid grid-cols-12 grid-rows-6 w-screen h-36 place-content-center place-items-center bg-secondary text-white text-center font-montserrat px-32 sticky top-0">
+        <header className={`${$headerClassName} ${style.header}`}>
             <HeaderBrand className={`col-start-1 col-end-3 ${$rowClassName}`} />
             <HeaderNavigation
                 className={`col-start-4 col-end-8 ${$rowClassName}`}
