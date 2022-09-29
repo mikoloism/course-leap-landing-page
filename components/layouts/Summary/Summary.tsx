6d294d78626779428829d520e08d06e4cc157327
mikoloism <mikoloism@gmail.com>
2022-09-29 22:15:32 +0330

    refactor(component): extract components from `summary` layouts to `organisms` (`SummaryHeader` and `SummaryCover`) components;

--- components/layouts/Summary/Summary.tsx
+++ components/layouts/Summary/Summary.tsx
@@ -1,54 +1,14 @@
-import { Group, Icon, Image, Link, Separator, Text } from '@/components/atoms';
-import { Header } from '@/molecules/Header';
-import style from '@/styles/layouts/summary.module.css';
-
-const $content = {
-    title: 'Featured Products',
-    description: `
-	Problems trying to resolve the conflict between
-	the two major realms of Classical physics:
-	Newtonian mechanics`,
-    cta: `Learn More`,
-    cover: '/assets/images/image-2.png',
-};
+import { Group } from '@/atoms';
+import { SummaryCover, SummaryHeader } from '@/organisms/Summary';
 
 export default function Summary() {
     return (
-        <Group className="w-screen h-[135vh] py-48 bg-white">
-            <Group
-                placeItems="start"
-                className="w-1/2 h-full order-1">
-                <Header
-                    level="h2"
-                    title={$content.title}
-                    description={$content.description}
-                    className="w-1/2 h-1/2 flex flex-wrap flex-col place-items-start place-content-center gap-y-12"
-                    Before={() => <Separator heavy />}
-                />
-
-                <Link
-                    href="/#"
-                    className="rounded-full h-20 w-56 bg-transparent inline-flex flex-wrap flex-row place-items-center place-content-center justify-start fill-primary">
-                    <Text
-                        size="link"
-                        color="primary"
-                        placeContent="center"
-                        className="w-fit h-full">
-                        {$content.cta}
-                    </Text>
-                    <Icon
-                        name="chevron-right"
-                        className="w-[20%] h-full overflow-hidden"
-                        scale="0.8"
-                    />
-                </Link>
-            </Group>
-            <Image
-                className={`next-image cover cover--2 col-start-7 col-end-13 row-start-1 row-end-7 w-1/2 h-full ${style.summaryCoverWrapper}`}
-                src={$content.cover}
-                alt="image-cover-1"
-                imageClassName={style.summaryCoverImage}
-            />
+        <Group
+            layout
+            dir="row"
+            color="white">
+            <SummaryHeader />
+            <SummaryCover />
         </Group>
     );
 }
