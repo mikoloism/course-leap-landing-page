f1c697336ca72475efdfb80f5daed25fe89eff71
mikoloism <mikoloism@gmail.com>
2022-10-08 18:53:13 +0330

    fix(component): extract all function out of the `Group` component to `Group` method as internal and private;
    remove unnecessary types; to make `SRP` each method do one things;

--- components/atoms/Group/Group.tsx
+++ components/atoms/Group/Group.tsx
@@ -1,7 +1,5 @@
-import { $, Component, PropsWithCommon, PropsWithStyle } from '@/libs/common';
+import { $, Component, PropsWithAll } from '@/libs/common';
 import {
-    ExactFlexDisplay,
-    ExactGridDisplay,
     getBackgroundColor,
     getFlexDirection,
     getFlexDisplay,
@@ -15,77 +13,88 @@ import {
 } from '@/libs/constants';
 import type {
     ColorType,
-    DirectionType,
-    FlexType,
-    GridType,
+    DisplayType,
+    PlaceCommon,
+    PlaceContent,
+    PlaceItems,
     PropsType,
 } from '@/libs/types';
 
-function getDisplayType(
-    type: GridType | FlexType
-): ExactFlexDisplay | ExactGridDisplay {
-    return isFlex(type)
-        ? getFlexDisplay(type)
-        : isGrid(type)
-        ? getGridDisplay(type)
-        : 'flex';
-}
-
-function withDisplayFlex({
-    dir,
-    nowrap,
-}: {
-    dir?: DirectionType;
-    nowrap?: boolean;
-}): string {
-    return $(getFlexDirection(dir), { 'flex-wrap': !nowrap });
-}
-
-function withDisplayGrid({
-    columns,
-    rows,
-}: {
-    columns?: number | string;
-    rows?: number | string;
-}): string {
-    return $('grid-cols-' + columns, 'grid-rows-' + rows);
-}
-
-type Props = PropsWithCommon<PropsWithStyle<PropsType>> & {
+type Props = PropsWithAll<PropsType> & {
     layout?: boolean;
     color?: ColorType;
 };
 
 export class Group extends Component<Props> {
+    private static DEFAULT_DISPLAY_TYPE: DisplayType = 'flex';
+    private static DEFAULT_PLACE_ITEMS: PlaceCommon | PlaceItems = 'center';
+    private static DEFAULT_PLACE_CONTENT: PlaceCommon | PlaceContent = 'center';
+    private $className: string | undefined;
+    private $displayType: DisplayType = Group.DEFAULT_DISPLAY_TYPE;
+
     constructor(props: Props) {
         super(props);
+        this.getStyleFromClassName();
     }
 
-    render() {
-        const $type = this.props.type ?? 'flex';
-        const $className = $(
-            getDisplayType($type),
-            isFlex($type)
-                ? withDisplayFlex({
-                      dir: this.props.dir,
-                      nowrap: (this.props as any).nowrap,
-                  })
-                : withDisplayGrid({
-                      columns: (this.props as any).columns,
-                      rows: (this.props as any).rows,
-                  }),
-            getGapSize({ gapx: this.props.gapx, gapy: this.props.gapy }),
-            getPlaceContent(this.props.placeContent, 'center'),
-            getPlaceItems(this.props.placeItems, 'center'),
+    private getDisplayTypeClassName(): string {
+        return isFlex(this.$displayType)
+            ? getFlexDisplay(this.$displayType)
+            : isGrid(this.$displayType)
+            ? getGridDisplay(this.$displayType)
+            : Group.DEFAULT_DISPLAY_TYPE;
+    }
+
+    private getDisplayFlexClassName(): string {
+        return $(getFlexDirection(this.props.dir), {
+            'flex-wrap': !(this.props as any).nowrap,
+        });
+    }
+
+    private getDisplayGridClassName(): string {
+        return $(
+            'grid-cols-' + (this.props as any).columns,
+            'grid-rows-' + (this.props as any).rows
+        );
+    }
+
+    private getClassNameByDisplay(): string {
+        return isFlex(this.$displayType)
+            ? this.getDisplayFlexClassName()
+            : this.getDisplayGridClassName();
+    }
+
+    private getGapSizeClassName(): string {
+        return getGapSize({ gapx: this.props.gapx, gapy: this.props.gapy });
+    }
+
+    private getPlaceAlignClassName(): string {
+        return $(
+            getPlaceContent(
+                this.props.placeContent,
+                Group.DEFAULT_PLACE_CONTENT
+            ),
+            getPlaceItems(this.props.placeItems, Group.DEFAULT_PLACE_ITEMS)
+        );
+    }
+
+    private getStyleFromClassName(): void {
+        this.$className = $(
+            this.getClassNameByDisplay(),
+            this.getDisplayTypeClassName(),
+            this.getGapSizeClassName(),
+            this.getPlaceAlignClassName(),
             this.props.color && getBackgroundColor(this.props.color),
             this.props.layout && 'w-screen h-screen p-0',
             this.props.pos && getPosition(this.props.pos),
             this.props.className
         );
+    }
 
+    public render() {
         return (
             <section
-                className={$className}
+                className={this.$className}
                 style={this.props.style ?? {}}>
                 {this.props.children}
             </section>
