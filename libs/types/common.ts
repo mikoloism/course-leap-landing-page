6341ebfad9cfd5b5a6fc5c03c7457d777911b4b7
mikoloism <mikoloism@gmail.com>
2022-10-09 20:32:45 +0330

    refactor(type): extract types in `types/style.ts` to separate type file;
    rename `types/style.ts` to `types/common.ts` file

---
+++ libs/types/common.ts
@@ -0,0 +1,5 @@
+import type { Gap } from './gap-size';
+import type { PlaceAlign } from './place-align-type';
+import type { Position } from './position';
+
+export type CommonProps = Position.Props & PlaceAlign.Props & Gap.Props;
