5e629f38db8c8ea7af80359b0f9659c6a7253de2
mikoloism <mikoloism@gmail.com>
2022-10-11 16:25:32 +0330

    perf(component): remove unneccary types and function from import statement in `Group` atomic component;
    add `private` method to `calculating style from classnames; replace constants function by `module` from constants;

--- components/atoms/Group/Group.tsx
+++ components/atoms/Group/Group.tsx
@@ -1,139 +1,109 @@
+import { $, Component, PropsWithAll, PropsWithClassName } from '@/libs/common';
 import {
-    $,
-    Component,
-    PropsWithAll,
-    PropsWithClassName,
-    PropsWithCommon,
-} from '@/libs/common';
-import {
-    getBackgroundColor,
-    getFlexDirection,
-    getFlexDisplay,
-    getGapSize,
-    getGridDisplay,
-    getPlaceAlign,
-    getPosition,
-    isFlex,
-    isGrid,
+    Color,
+    CommonProps,
+    Display,
+    Gap,
+    PlaceAlign,
+    Position,
 } from '@/libs/constants';
-import type {
-    ColorType,
-    DisplayType,
-    PlaceCommon,
-    PlaceContent,
-    PlaceItems,
-    PropsType,
-} from '@/libs/types';
+import { GroupRender } from './GroupRender';
 
-type StyleProps = PropsWithClassName<PropsType> & {
+type StyleProps = PropsWithClassName<
+    CommonProps & Color.Props & Display.Props
+> & {
     layout?: boolean;
-    color?: ColorType;
 };
 
 type Props = PropsWithAll<StyleProps>;
 
-class GroupComponent extends Component<PropsWithAll<{}>> {
-    constructor(props: PropsWithCommon<{}>) {
-        super(props);
-    }
-
-    private RenderWithStyleProps(props: PropsWithAll<{}>) {
-        return (
-            <section
-                className={props.className}
-                style={props.style}>
-                {props.children}
-            </section>
-        );
-    }
-
-    private RenderWithoutStyleProps(props: PropsWithCommon<{}>) {
-        return <section className={props.className}>{props.children}</section>;
-    }
-
-    private hasStyleProps(): boolean {
-        return !!this.props?.style;
-    }
-
-    render() {
-        const RenderComponent = this.hasStyleProps()
-            ? this.RenderWithStyleProps
-            : this.RenderWithoutStyleProps;
-
-        return <RenderComponent {...this.props} />;
-    }
-}
-
 export class Group extends Component<Props> {
-    private static DEFAULT_DISPLAY_TYPE: DisplayType = 'flex';
-    private static DEFAULT_PLACE_ITEMS: PlaceCommon | PlaceItems = 'center';
-    private static DEFAULT_PLACE_CONTENT: PlaceCommon | PlaceContent = 'center';
-    private $displayType: DisplayType = Group.DEFAULT_DISPLAY_TYPE;
-
     constructor(props: Props) {
         super(props);
-
-        if (props.type) this.$displayType = props.type;
     }
 
-    private getDisplayTypeClassName(): string {
-        return isFlex(this.$displayType)
-            ? getFlexDisplay(this.$displayType)
-            : isGrid(this.$displayType)
-            ? getGridDisplay(this.$displayType)
-            : Group.DEFAULT_DISPLAY_TYPE;
-    }
+    public render() {
+        const className = this.getStyleFromClassName();
 
-    private getDisplayFlexClassName(): string {
-        return $(getFlexDirection(this.props.dir), {
-            'flex-wrap': !(this.props as any).nowrap,
-        });
-    }
-
-    private getDisplayGridClassName(): string {
-        return $(
-            'grid-cols-' + (this.props as any).columns,
-            'grid-rows-' + (this.props as any).rows
+        return (
+            <GroupRender
+                className={className}
+                style={this.props.style ?? {}}>
+                {this.props.children}
+            </GroupRender>
         );
-    }
-
-    private getClassNameByDisplay(): string {
-        return isFlex(this.$displayType)
-            ? this.getDisplayFlexClassName()
-            : this.getDisplayGridClassName();
-    }
-
-    private getGapSizeClassName(): string {
-        return getGapSize({ gapx: this.props.gapx, gapy: this.props.gapy });
-    }
-
-    private getPlaceAlignClassName(): string {
-        return getPlaceAlign({
-            content: this.props.placeContent ?? Group.DEFAULT_PLACE_CONTENT,
-            items: this.props.placeItems ?? Group.DEFAULT_PLACE_ITEMS,
-        });
     }
 
     private getStyleFromClassName(): string {
         return $(
+            this.getBackgroundColorClassName(),
             this.getClassNameByDisplay(),
-            this.getDisplayTypeClassName(),
             this.getGapSizeClassName(),
+            this.getLayoutModeClassName(),
             this.getPlaceAlignClassName(),
-            this.props.color && getBackgroundColor(this.props.color),
-            this.props.layout && 'w-screen h-screen p-0',
-            this.props.pos && getPosition(this.props.pos),
+            this.getPositionsClassName(),
             this.props.className
         );
     }
 
-    public render() {
-        return (
-            <GroupComponent
-                className={this.getStyleFromClassName()}
-                style={this.props.style ?? {}}>
-                {this.props.children}
-            </GroupComponent>
-        );
+    private getBackgroundColorClassName(): string {
+        return this.props.color
+            ? Color.Background.getClassName(this.props.color)
+            : '';
+    }
+
+    private getClassNameByDisplay(): string | '' {
+        const type = this.props?.type ?? Display.Flex.DEFAULT_KEY;
+
+        if (Display.Grid.isDisplayGrid(type)) {
+            const props = this.props as Display.Grid.Props;
+
+            return Display.getClassNameByDisplayGrid({
+                type: props.type,
+                columns: props.columns,
+                rows: props.rows,
+            });
+        }
+
+        if (Display.Flex.isDisplayFlex(type)) {
+            const props = this.props as Display.Flex.Props;
+
+            return Display.getClassNameByDisplayFlex({
+                type: props.type,
+                dir: props.dir,
+                nowrap: props.nowrap,
+            });
+        }
+
+        return '';
+    }
+
+    private getGapSizeClassName(): string {
+        return Gap.getClassName({
+            gapx: this.props.gapx,
+            gapy: this.props.gapy,
+        });
+    }
+
+    private getLayoutModeClassName(): string {
+        const LAYOUT_MODE_CLASSNAME = 'w-screen h-screen p-0';
+
+        return this.props.layout ? LAYOUT_MODE_CLASSNAME : '';
+    }
+
+    private getPlaceAlignClassName(): string {
+        const DEFAULT_PLACE_CONTENT = 'center';
+        const DEFAULT_PLACE_ITEMS = 'center';
+
+        return PlaceAlign.getClassName({
+            content: this.props.placeContent ?? DEFAULT_PLACE_CONTENT,
+            items: this.props.placeItems ?? DEFAULT_PLACE_ITEMS,
+        });
+    }
+
+    private getPositionsClassName(): string {
+        return this.props.pos
+            ? (Position.getClassName(this.props.pos) as string)
+            : '';
     }
 }
