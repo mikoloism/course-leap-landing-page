5564b9800571438522ff5d19983cea0a4013ca07
mikoloism <mikoloism@gmail.com>
2022-10-11 16:18:25 +0330

    fix(util): add `getClassName` function to `Display` module in `constants/display/common.ts` file;

--- libs/constants/display/common.ts
+++ libs/constants/display/common.ts
@@ -13,3 +13,44 @@ export module Display {
     export import Flex = _Flex;
     export import Grid = _Grid;
 }
+
+export module Display {
+    type GetByDisplayArg = GetByFlexArg | GetByGridArg;
+    type GetByFlexArg = Flex.Props;
+    type GetByGridArg = Grid.Props;
+
+    export function getClassNameByDisplay({
+        type,
+        ...props
+    }: GetByDisplayArg): string {
+        if (Flex.isDisplayFlex(type)) {
+            return getClassNameByDisplayFlex({ type, ...props });
+        }
+
+        if (Grid.isDisplayGrid(type)) {
+            return getClassNameByDisplayGrid({ type, ...props });
+        }
+
+        return getClassNameByDisplayFlex({ type: 'flex', ...props });
+    }
+
+    export function getClassNameByDisplayFlex({
+        type,
+        dir,
+        nowrap,
+    }: GetByFlexArg): string {
+        return [
+            Flex.getClassName(type),
+            Flex.getDirectionClassName(dir),
+            Flex.getNoWrapClassName(nowrap),
+        ].join(' ');
+    }
+
+    export function getClassNameByDisplayGrid({
+        type,
+        columns,
+        rows,
+    }: GetByGridArg): string {
+        return Grid.getClassName(type) as string;
+    }
+}
