0e843cb489abff05413cafa7e1469428fd0326f7
mikoloism <mikoloism@gmail.com>
2022-10-13 11:11:59 +0330

    feat(util): add `StyleFactory` class to `constants/place-align/`

--- libs/constants/place-align/common.ts
+++ libs/constants/place-align/common.ts
@@ -25,10 +25,50 @@ export module PlaceAlign {
         content,
         items,
     }: PlaceAlignArgs): ClassName {
-        return classnames(
-            PlaceContent.getClassName(content),
-            PlaceItems.getClassName(items)
-        ) as ClassName;
+        return new StyleFactory(content, items).createClassName();
+    }
+
+    export const createPlaceContentClassName = PlaceContent.getClassName;
+
+    export const createPlaceItemsClassName = PlaceItems.getClassName;
+
+    export class StyleFactory {
+        protected DEFAULT_PLACE_CONTENT: PlaceContent.Keys = 'start' as const;
+        protected DEFAULT_PLACE_ITEMS: PlaceItems.Keys = 'start' as const;
+
+        public constructor(
+            private placeContent: PlaceContent.Keys | undefined,
+            private placeItems: PlaceItems.Keys | undefined
+        ) {}
+
+        public createClassName(): ClassName {
+            return classnames(
+                this.createPlaceItemsClassName(),
+                this.createPlaceContentClassName()
+            ) as ClassName;
+        }
+
+        private createPlaceContentClassName(): PlaceContent.ClassName {
+            return createPlaceContentClassName(
+                this.getPlaceContent(),
+                this.DEFAULT_PLACE_CONTENT
+            );
+        }
+
+        private getPlaceContent(): PlaceContent.Keys {
+            return this?.placeContent ?? this.DEFAULT_PLACE_CONTENT;
+        }
+
+        private createPlaceItemsClassName(): PlaceItems.ClassName {
+            return createPlaceItemsClassName(
+                this.getPlaceItems(),
+                this.DEFAULT_PLACE_ITEMS
+            );
+        }
+
+        private getPlaceItems(): PlaceItems.Keys {
+            return this?.placeItems ?? this.DEFAULT_PLACE_ITEMS;
+        }
     }
 }
 
