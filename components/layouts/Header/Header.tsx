c7b72649d23b227d0ca6377cd8a99ce4877000d0
mikoloism <mikoloism@gmail.com>
2022-10-15 09:51:12 +0330

    perf(component): convert functional component to class component;

--- components/layouts/Header/Header.tsx
+++ components/layouts/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { Component } from '@/libs/common';
 import {
     HeaderActionGroup,
     HeaderBrand,
@@ -5,20 +6,28 @@ import {
 } from '@/organisms/Header';
 import style from '@/styles/header.module.css';
 
-export default function Header() {
-    const $headerClassName =
-        'grid grid-cols-12 grid-rows-6 place-content-center place-items-center w-screen bg-secondary text-white px-32';
-    const $rowClassName = 'row-start-2 row-end-6';
+export default class HeaderComponent extends Component {
+    render() {
+        return (
+            <header className={this.getClassName()}>
+                <HeaderBrand
+                    className={`col-start-1 col-end-3 ${this.getRowClassName()}`}
+                />
+                <HeaderNavigation
+                    className={`col-start-4 col-end-8 ${this.getRowClassName()}`}
+                />
+                <HeaderActionGroup
+                    className={`col-start-10 col-end-13 ${this.getRowClassName()}`}
+                />
+            </header>
+        );
+    }
 
-    return (
-        <header className={`${$headerClassName} ${style.header}`}>
-            <HeaderBrand className={`col-start-1 col-end-3 ${$rowClassName}`} />
-            <HeaderNavigation
-                className={`col-start-4 col-end-8 ${$rowClassName}`}
-            />
-            <HeaderActionGroup
-                className={`col-start-10 col-end-13 ${$rowClassName}`}
-            />
-        </header>
-    );
+    private getClassName(): string {
+        return `grid grid-cols-12 grid-rows-6 place-content-center place-items-center w-screen bg-secondary text-white px-32 ${style.header}`;
+    }
+
+    private getRowClassName() {
+        return 'row-start-2 row-end-6';
+    }
 }
--- components/organisms/Header/Navigation.tsx
+++ components/organisms/Header/Navigation.tsx
@@ -1,34 +1,58 @@
 import { Item, Link, List } from '@/atoms';
-import { PropsWithClassName } from '@/libs/common';
-import { useContent } from '@/libs/hooks';
+import { Component, PropsWithClassName } from '@/libs/common';
+import { useContent as retrieveContent } from '@/libs/hooks';
 
-type NavigationItemProps = { text: string };
+type Props = PropsWithClassName;
 
-function NavigationItem({ text }: NavigationItemProps) {
-    return <Link href={`#${text ?? ''}`}>{text}</Link>;
-}
+type RenderItemProps = { text: string };
 
-export function HeaderNavigation({ className }: PropsWithClassName<{}>) {
-    const { content } = useContent('landing-page', 'header_navigation');
-    const navigation = [
-        { text: content('home') },
-        { text: content('product') },
-        { text: content('pricing') },
-        { text: content('contact') },
-    ];
+export class HeaderNavigation extends Component<Props> {
+    public constructor(props: Props) {
+        super(props);
+    }
 
-    return (
-        <nav
-            className={`flex-wrap flex-row inline-flex place-content-center place-items-center w-full h-full ${className}`}>
-            <List
-                data={navigation}
-                dir="row"
-                className="w-full h-full space-x-0 space-y-0 p-0"
-                Component={Item<NavigationItemProps>(
-                    NavigationItem,
-                    'w-1/4 h-full list-none flex flex-col flex-wrap place-justify-center place-content-center font-montserrat text-link'
-                )}
-            />
-        </nav>
-    );
+    public render() {
+        return (
+            <nav className={this.getClassName()}>
+                <List
+                    data={this.getItems()}
+                    dir="row"
+                    className="w-full h-full space-x-0 space-y-0 p-0"
+                    Component={this.renderItem()}
+                />
+            </nav>
+        );
+    }
+
+    private getClassName(): string {
+        return `flex-wrap flex-row inline-flex place-content-center place-items-center w-full h-full ${this.props.className}`;
+    }
+
+    private getItems(): Array<{ text: string }> {
+        const { content } = retrieveContent(
+            'landing-page',
+            'header_navigation'
+        );
+
+        return [
+            { text: content('home') },
+            { text: content('product') },
+            { text: content('pricing') },
+            { text: content('contact') },
+        ];
+    }
+
+    private renderItem() {
+        return Item<RenderItemProps>(({ text }: RenderItemProps) => {
+            const href = text ?? '';
+
+            return (
+                <Link
+                    className="group"
+                    href={`#${href}`}>
+                    {text}
+                </Link>
+            );
+        }, 'w-1/4 h-full list-none flex flex-col flex-wrap place-justify-center place-content-center font-montserrat text-link');
+    }
 }
