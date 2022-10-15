7713fed970d3e75d3560d61eef3ee53a913e5873
mikoloism <mikoloism@gmail.com>
2022-10-15 11:03:18 +0330

    fix(component): replace (`more button` component) custom component from `achievement header` component;

--- components/organisms/AchievementHeader/AchievementHeader.tsx
+++ components/organisms/AchievementHeader/AchievementHeader.tsx
@@ -1,12 +1,10 @@
-import { Group, Icon, Link, Separator, Text } from '@/atoms';
+import { Group, Separator } from '@/atoms';
 import { useContent } from '@/libs/hooks';
 import { Header } from '@/molecules/Header';
+import { MoreButton } from '@/molecules/MoreButton';
 
 export function AchievementHeader() {
     const { content } = useContent('landing-page', 'achievements');
-    const $_call_to_action_$ = useContent('landing-page', 'common').content(
-        'call_to_action'
-    );
 
     return (
         <Group
@@ -19,21 +17,8 @@ export function AchievementHeader() {
                 Before={() => <Separator heavy />}
                 className="w-1/2 h-1/2 flex flex-wrap flex-col place-items-start place-content-center gap-y-12"
             />
-            <Link
-                href="#"
-                className="rounded-full h-20 w-56 bg-transparent text-link inline-flex flex-wrap flex-row place-items-center place-content-center justify-start fill-primary text-primary">
-                <Text
-                    color="primary"
-                    placeContent="center"
-                    className="w-fit h-full">
-                    {$_call_to_action_$}
-                </Text>
-                <Icon
-                    name="chevron-right"
-                    className="w-[20%] h-full overflow-hidden"
-                    scale="0.8"
-                />
-            </Link>
+
+            <MoreButton />
         </Group>
     );
 }
