20c0b7cbfdf4df4e6fd46b971cefd5890e2b5d0f
mikoloism <mikoloism@gmail.com>
2022-10-15 11:04:17 +0330

    fix(component): add tailwind `hover` effect classnames

--- components/molecules/MoreButton/MoreButton.tsx
+++ components/molecules/MoreButton/MoreButton.tsx
@@ -11,24 +11,26 @@ export function MoreButton({ href, text, outline }: Props) {
     const { content } = useContent('landing-page', 'common');
     href ??= '#';
     text ??= content('call_to_action');
-    const $className = `rounded-full h-20 w-56 bg-transparent inline-flex flex-wrap flex-row place-items-center place-content-center fill-primary ${
-        outline ? 'border border-primary' : 'justify-start'
-    }`;
+    const className = `
+    hover:scale-105 transform transition
+    group-hover:bg-primary group-hover:fill-white
+    rounded-full h-20 w-56 bg-transparent inline-flex flex-wrap flex-row place-items-center place-content-center fill-primary
+    ${outline ? 'border border-primary' : 'justify-start'}`.trim();
 
     return (
         <Link
             href={href ?? '#'}
-            className={$className}>
+            className={className}>
             <Text
                 size="link"
                 color="primary"
                 placeContent="center"
-                className="w-fit h-full">
+                className="w-fit group-hover:text-white transition">
                 {text}
             </Text>
             <Icon
                 name="chevron-right"
-                className="w-[20%] h-full overflow-hidden"
+                className="w-[20%] h-full overflow-hidden transition"
                 scale="0.8"
             />
         </Link>
