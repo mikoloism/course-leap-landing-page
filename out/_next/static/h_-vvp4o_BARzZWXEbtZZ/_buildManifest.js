a2ba81b37b4eea5fbd732ee427ba0b26c9500797
mikoloism <mikoloism@gmail.com>
2022-10-18 00:10:15 +0330

    build(deploy) : deploy by next.js methodology for gh-pages

---
+++ out/_next/static/h_-vvp4o_BARzZWXEbtZZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/573-d19882cad95e5837.js","static/css/38906ed53fdb739f.css","static/chunks/pages/index-dd3b96f33add8fc3.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
