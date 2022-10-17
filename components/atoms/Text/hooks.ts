472292cd9c3639ecd591441fdbed2e000cdaed18
mikoloism <mikoloism@gmail.com>
2022-10-17 20:11:13 +0330

    fix(hook): remove deprecated hook file from `text` component

--- components/atoms/Text/hooks.ts
+++
@@ -1,7 +0,0 @@
-import type { AsPropsType } from './types';
-
-const TextComponents: Record<AsPropsType, React.ReactNode> | null = null;
-
-export function getTextComponent(type: AsPropsType) {
-    return TextComponents?.[type];
-}
