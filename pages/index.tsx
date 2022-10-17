772e86bd9f22ae39eb043d3d02650207685a13e8
mikoloism <mikoloism@gmail.com>
2022-10-17 21:58:35 +0330

    fix(page): remove `ssg` method from `index.tsx` page;

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,3 @@ import LandingPageTemplate from '@/templates/LandingPage';
 export default function LandingPage() {
     return <LandingPageTemplate />;
 }
-
-export async function getStaticProps() {
-    return { props: {} };
-}
