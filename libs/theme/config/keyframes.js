e1092000c34748300e4092479da09c61e8888623
mikoloism <mikoloism@gmail.com>
2022-10-18 10:59:44 +0330

    feat(util): add `heart-beat` animation to `keyfrmaes` and `animation` config file for `tailwin.config.js`;

--- libs/theme/config/keyframes.js
+++ libs/theme/config/keyframes.js
@@ -4,9 +4,15 @@ exports.keyframes = {
         '90%': { transform: 'translate(0, 0)' },
         'to, 100%': { opacity: 1 },
     },
+
     'fade-in-down': {
         'from, 0%': { opacity: 0, transform: 'translate(0, -50%)' },
         '90%': { transform: 'translate(0, 0)' },
         'to, 100%': { opacity: 1 },
+    },
+
+    'heart-beat': {
+        '0%, 40%, 80%, 100%': { transform: 'scale(0.98)' },
+        '20%, 60%': { transform: 'scale(1)' },
     },
 };
