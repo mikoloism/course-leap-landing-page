2ae25fcea8a1c71a6e95722f0a5da18bb3849840
mikoloism <mikoloism@gmail.com>
2022-10-19 15:11:18 +0330

    fix(util): remove `duplicated` slashes between image `src` and  `base image path` variable on `loadImage` funciton;

--- components/atoms/Image/image-loader.ts
+++ components/atoms/Image/image-loader.ts
@@ -1,5 +1,5 @@
 const BASE_IMAGE_PATH: string = `/course-leap-landing-page/assets/images`;
 
 export function loadImage({ src }: any) {
-    return `${BASE_IMAGE_PATH}/${src.replace(BASE_IMAGE_PATH, '')}`;
+    return `${BASE_IMAGE_PATH}${src.replace(`${BASE_IMAGE_PATH}/`, '')}`;
 }
