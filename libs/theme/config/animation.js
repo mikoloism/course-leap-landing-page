9c52d88742cbbb3fcab1c66530d74ed36f63ed7d
mikoloism <mikoloism@gmail.com>
2022-10-20 10:53:27 +0330

    feat(style): add `push-out-up` and `push-in-down` animation and keyframes to `tailwind.config.js` in `theme/configs/`;

--- libs/theme/config/animation.js
+++ libs/theme/config/animation.js
@@ -2,4 +2,6 @@ exports.animation = {
     'fade-in-up': 'fade-in-up 0.9s 0.1s ease both',
     'fade-in-down': 'fade-in-down 0.9s 0.1s ease both',
     'heart-beat': 'heart-beat 1s infinite',
+    'push-out-up': 'push-out-up 1s 0.1s ease both',
+    'push-in-down': 'push-in-down 1s 0.1s ease both',
 };
--- libs/theme/config/keyframes.js
+++ libs/theme/config/keyframes.js
@@ -15,4 +15,14 @@ exports.keyframes = {
         '0%, 40%, 80%, 100%': { transform: 'scale(0.98)' },
         '20%, 60%': { transform: 'scale(1)' },
     },
+
+    'push-out-up': {
+        'from, 0%': { transform: 'translateY(0)' },
+        'to, 100%': { transform: 'translateY(-50%)' },
+    },
+
+    'push-in-down': {
+        'from, 0%': { transform: 'translateY(-50%)' },
+        'to, 100%': { transform: 'translateY(0)' },
+    },
 };
